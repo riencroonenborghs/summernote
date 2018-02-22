7dcd885a3953a139fb7c669c7620163dd8246d1d
Rien Croonenborghs <rien@croonenborghs.net>
2018-02-23 09:04:34 +1300

    disable non-youtube video

--- src/js/base/module/VideoDialog.js
+++ src/js/base/module/VideoDialog.js
@@ -52,35 +52,35 @@ export default class VideoDialog {
     const ytRegExp = /^(?:https?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?.+&v=))((\w|-){11})(?:\S+)?$/;
     const ytMatch = url.match(ytRegExp);
 
-    const igRegExp = /(?:www\.|\/\/)instagram\.com\/p\/(.[a-zA-Z0-9_-]*)/;
-    const igMatch = url.match(igRegExp);
+    // const igRegExp = /(?:www\.|\/\/)instagram\.com\/p\/(.[a-zA-Z0-9_-]*)/;
+    // const igMatch = url.match(igRegExp);
 
-    const vRegExp = /\/\/vine\.co\/v\/([a-zA-Z0-9]+)/;
-    const vMatch = url.match(vRegExp);
+    // const vRegExp = /\/\/vine\.co\/v\/([a-zA-Z0-9]+)/;
+    // const vMatch = url.match(vRegExp);
 
-    const vimRegExp = /\/\/(player\.)?vimeo\.com\/([a-z]*\/)*(\d+)[?]?.*/;
-    const vimMatch = url.match(vimRegExp);
+    // const vimRegExp = /\/\/(player\.)?vimeo\.com\/([a-z]*\/)*(\d+)[?]?.*/;
+    // const vimMatch = url.match(vimRegExp);
 
-    const dmRegExp = /.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/;
-    const dmMatch = url.match(dmRegExp);
+    // const dmRegExp = /.+dailymotion.com\/(video|hub)\/([^_]+)[^#]*(#video=([^_&]+))?/;
+    // const dmMatch = url.match(dmRegExp);
 
-    const youkuRegExp = /\/\/v\.youku\.com\/v_show\/id_(\w+)=*\.html/;
-    const youkuMatch = url.match(youkuRegExp);
+    // const youkuRegExp = /\/\/v\.youku\.com\/v_show\/id_(\w+)=*\.html/;
+    // const youkuMatch = url.match(youkuRegExp);
 
-    const qqRegExp = /\/\/v\.qq\.com.*?vid=(.+)/;
-    const qqMatch = url.match(qqRegExp);
+    // const qqRegExp = /\/\/v\.qq\.com.*?vid=(.+)/;
+    // const qqMatch = url.match(qqRegExp);
 
-    const qqRegExp2 = /\/\/v\.qq\.com\/x?\/?(page|cover).*?\/([^\/]+)\.html\??.*/;
-    const qqMatch2 = url.match(qqRegExp2);
+    // const qqRegExp2 = /\/\/v\.qq\.com\/x?\/?(page|cover).*?\/([^\/]+)\.html\??.*/;
+    // const qqMatch2 = url.match(qqRegExp2);
 
-    const mp4RegExp = /^.+.(mp4|m4v)$/;
-    const mp4Match = url.match(mp4RegExp);
+    // const mp4RegExp = /^.+.(mp4|m4v)$/;
+    // const mp4Match = url.match(mp4RegExp);
 
-    const oggRegExp = /^.+.(ogg|ogv)$/;
-    const oggMatch = url.match(oggRegExp);
+    // const oggRegExp = /^.+.(ogg|ogv)$/;
+    // const oggMatch = url.match(oggRegExp);
 
-    const webmRegExp = /^.+.(webm)$/;
-    const webmMatch = url.match(webmRegExp);
+    // const webmRegExp = /^.+.(webm)$/;
+    // const webmMatch = url.match(webmRegExp);
 
     let $video;
     if (ytMatch && ytMatch[1].length === 11) {
@@ -88,50 +88,55 @@ export default class VideoDialog {
       $video = $('<iframe>')
         .attr('frameborder', 0)
         .attr('src', '//www.youtube.com/embed/' + youtubeId);
-    } else if (igMatch && igMatch[0].length) {
-      $video = $('<iframe>')
-        .attr('frameborder', 0)
-        .attr('src', 'https://instagram.com/p/' + igMatch[1] + '/embed/')
-        .attr('width', '612').attr('height', '710')
-        .attr('scrolling', 'no')
-        .attr('allowtransparency', 'true');
-    } else if (vMatch && vMatch[0].length) {
-      $video = $('<iframe>')
-        .attr('frameborder', 0)
-        .attr('src', vMatch[0] + '/embed/simple')
-        .attr('width', '600').attr('height', '600')
-        .attr('class', 'vine-embed');
-    } else if (vimMatch && vimMatch[3].length) {
-      $video = $('<iframe webkitallowfullscreen mozallowfullscreen allowfullscreen>')
-        .attr('frameborder', 0)
-        .attr('src', '//player.vimeo.com/video/' + vimMatch[3])
-        .attr('width', '640').attr('height', '360');
-    } else if (dmMatch && dmMatch[2].length) {
-      $video = $('<iframe>')
-        .attr('frameborder', 0)
-        .attr('src', '//www.dailymotion.com/embed/video/' + dmMatch[2])
-        .attr('width', '640').attr('height', '360');
-    } else if (youkuMatch && youkuMatch[1].length) {
-      $video = $('<iframe webkitallowfullscreen mozallowfullscreen allowfullscreen>')
-        .attr('frameborder', 0)
-        .attr('height', '498')
-        .attr('width', '510')
-        .attr('src', '//player.youku.com/embed/' + youkuMatch[1]);
-    } else if ((qqMatch && qqMatch[1].length) || (qqMatch2 && qqMatch2[2].length)) {
-      const vid = ((qqMatch && qqMatch[1].length) ? qqMatch[1] : qqMatch2[2]);
-      $video = $('<iframe webkitallowfullscreen mozallowfullscreen allowfullscreen>')
-        .attr('frameborder', 0)
-        .attr('height', '310')
-        .attr('width', '500')
-        .attr('src', 'http://v.qq.com/iframe/player.html?vid=' + vid + '&amp;auto=0');
-    } else if (mp4Match || oggMatch || webmMatch) {
-      $video = $('<video controls>')
-        .attr('src', url)
-        .attr('width', '640').attr('height', '360');
     } else {
-      // this is not a known video link. Now what, Cat? Now what?
+      console.error('Only Youtube videos are allowed.');
+      alert('Only Youtube videos are allowed.');
       return false;
     }
+    // } else if (igMatch && igMatch[0].length) {
+    //   $video = $('<iframe>')
+    //     .attr('frameborder', 0)
+    //     .attr('src', 'https://instagram.com/p/' + igMatch[1] + '/embed/')
+    //     .attr('width', '612').attr('height', '710')
+    //     .attr('scrolling', 'no')
+    //     .attr('allowtransparency', 'true');
+    // } else if (vMatch && vMatch[0].length) {
+    //   $video = $('<iframe>')
+    //     .attr('frameborder', 0)
+    //     .attr('src', vMatch[0] + '/embed/simple')
+    //     .attr('width', '600').attr('height', '600')
+    //     .attr('class', 'vine-embed');
+    // } else if (vimMatch && vimMatch[3].length) {
+    //   $video = $('<iframe webkitallowfullscreen mozallowfullscreen allowfullscreen>')
+    //     .attr('frameborder', 0)
+    //     .attr('src', '//player.vimeo.com/video/' + vimMatch[3])
+    //     .attr('width', '640').attr('height', '360');
+    // } else if (dmMatch && dmMatch[2].length) {
+    //   $video = $('<iframe>')
+    //     .attr('frameborder', 0)
+    //     .attr('src', '//www.dailymotion.com/embed/video/' + dmMatch[2])
+    //     .attr('width', '640').attr('height', '360');
+    // } else if (youkuMatch && youkuMatch[1].length) {
+    //   $video = $('<iframe webkitallowfullscreen mozallowfullscreen allowfullscreen>')
+    //     .attr('frameborder', 0)
+    //     .attr('height', '498')
+    //     .attr('width', '510')
+    //     .attr('src', '//player.youku.com/embed/' + youkuMatch[1]);
+    // } else if ((qqMatch && qqMatch[1].length) || (qqMatch2 && qqMatch2[2].length)) {
+    //   const vid = ((qqMatch && qqMatch[1].length) ? qqMatch[1] : qqMatch2[2]);
+    //   $video = $('<iframe webkitallowfullscreen mozallowfullscreen allowfullscreen>')
+    //     .attr('frameborder', 0)
+    //     .attr('height', '310')
+    //     .attr('width', '500')
+    //     .attr('src', 'http://v.qq.com/iframe/player.html?vid=' + vid + '&amp;auto=0');
+    // } else if (mp4Match || oggMatch || webmMatch) {
+    //   $video = $('<video controls>')
+    //     .attr('src', url)
+    //     .attr('width', '640').attr('height', '360');
+    // } else {
+    //   // this is not a known video link. Now what, Cat? Now what?
+    //   return false;
+    // }
 
     $video.addClass('note-video-clip');
 
